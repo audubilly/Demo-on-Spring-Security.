c98e4c33075a4605c456085b65802d18cf364be7
audubilly <audubilly89@gmail.com>
2021-08-23 17:53:16 +0100

    permission based authentication, role based uthentication

--- src/main/java/com/example/demo/Student/StudentsManagementControler.java
+++ src/main/java/com/example/demo/Student/StudentsManagementControler.java
@@ -1,17 +1,45 @@
 package com.example.demo.Student;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
 import java.util.List;
 
 @RestController
-@RequestMapping
+@RequestMapping("management/api/v1/students ")
 public class StudentsManagementControler {
     private static final List<Student> STUDENTS = Arrays.asList(
             new Student(1,"billy audu"),
             new Student(2, "maria jones"),
             new Student(3,"annah smith")
     );
+
+    @GetMapping
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_ADMINTRAINEE')")
+    public List<Student> getAllStudents(){
+        System.out.println("getAllStudents");
+        return STUDENTS;
+    }
+
+    @PostMapping
+    @PreAuthorize("hasAuthority('student:write')")
+    public void registerNewStudents(@RequestBody  Student student){
+        System.out.println("registerNewStudents");
+        System.out.println(student);
+    }
+
+    @DeleteMapping(path = "{studentId}" )
+    @PreAuthorize("hasAuthority('student:write')")
+    public void deleteStudent(@PathVariable("studentId") Integer studentId){
+        System.out.println("deleteStudent");
+        System.out.println(studentId);
+    }
+
+    @PutMapping(path = "{studentId}")
+    @PreAuthorize("hasAuthority('student:write')")
+    public void updateStudent(@PathVariable("studentId") Integer studentId , @RequestBody Student student){
+        System.out.println("updateStudent");
+        System.out.println(String.format("%s %s", studentId, student));
+    }
 }
--- src/main/java/com/example/demo/security/ApplicationUserPermission.java
+++ src/main/java/com/example/demo/security/ApplicationUserPermission.java
@@ -2,9 +2,9 @@ package com.example.demo.security;
 
 public enum ApplicationUserPermission {
     STUDENT_READ("student:read"),
-    STUDENT_WRITE("student_write"),
-    COURSE_READ("course_read"),
-    COURSE_WRITE("course_write");
+    STUDENT_WRITE("student:write"),
+    COURSE_READ("course:read"),
+    COURSE_WRITE("course:write");
 
     private final String permission;
 

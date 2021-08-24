efe50bdb2d99c00d43760487725700a6fd923ffb
audubilly <audubilly89@gmail.com>
2021-08-24 09:22:04 +0100

    csrf token

--- src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
+++ src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
@@ -32,7 +32,7 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-//
+//                .csrf().disable() //TODO: do this
                 .authorizeRequests()
                 .antMatchers("/","index","/css/*", "/js/*").permitAll()
                 .antMatchers("/ api/**").hasAnyRole(STUDENT.name(), ADMIN.name())
